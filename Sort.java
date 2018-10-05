7a05324b8510f8216ad2ed38d704e1853f341553
Beardev <43851844+BeardevCode@users.noreply.github.com>
2018-10-06 00:51:42 +0200

    Added Bubble sort

---
+++ Sort.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+
+public class Sort {
+	
+	/**
+	 * Check if an array is sorted
+	 * 
+	 * @param tab Array of numbers
+	 * @return True or false
+	 */
+	private static boolean isSorted(int[] tab) {
+		if (tab.length == 0)
+			return true;
+		for (int i = 0; i < tab.length - 1; i++)
+			if (tab[i] > tab[i + 1])
+				return false;
+		return true;
+	}
+	
+	/**
+	 * Sort an array of numbers using Bubble sort
+	 * 
+	 * @param tab Array of numbers we want to sort
+	 * @param verbose Boolean, if we want to see the tab at every iteration
+	 * @param time Boolean, if we want to know the time it took.
+	 * @return Tab sorted
+	 */
+	public static int[] bubbleSort(int[] tab, boolean verbose, boolean time) {
+		int tmp = 0;
+		long startTime = System.currentTimeMillis();
+
+		while (isSorted(tab) == false) {
+			if (verbose)
+				System.out.println(Arrays.toString(tab));
+			for (int i = 0; i < tab.length - 1; i++) {
+				if (tab[i] > tab[i + 1]) {
+					tmp = tab[i];
+					tab[i] = tab[i + 1];
+					tab[i + 1] = tmp;
+				}
+			}
+		}
+		
+		long stopTime = System.currentTimeMillis();
+		long timeTaken = stopTime - startTime;
+		
+		if (verbose)
+			System.out.println(Arrays.toString(tab));
+		if (time)
+			System.out.println("Time: " + timeTaken + " ms");
+		
+		return tab;
+	}
+}
