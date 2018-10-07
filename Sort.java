07d0d719213dfdc50f97d1009ca5c30850b8efab
Beardev <43851844+BeardevCode@users.noreply.github.com>
2018-10-08 01:28:42 +0200

    Added Selection sort

--- Sort.java
+++ Sort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Sort {
 	
 	/**
-	 * Check if an array is sorted
+	 * Check if an array of is sorted
 	 * 
 	 * @param tab Array of numbers
 	 * @return True or false
@@ -29,14 +29,55 @@ public class Sort {
 		int tmp = 0;
 		long startTime = System.currentTimeMillis();
 
-		while (isSorted(tab) == false) {
-			if (verbose)
-				System.out.println(Arrays.toString(tab));
+		while (!isSorted(tab)) {
 			for (int i = 0; i < tab.length - 1; i++) {
+				if (verbose)
+					System.out.println(Arrays.toString(tab));
 				if (tab[i] > tab[i + 1]) {
 					tmp = tab[i];
 					tab[i] = tab[i + 1];
 					tab[i + 1] = tmp;
+				}
+			}
+		}
+		
+		long stopTime = System.currentTimeMillis();
+		long timeTaken = stopTime - startTime;
+		
+		if (verbose)
+			System.out.println(Arrays.toString(tab));
+		if (time)
+			System.out.println("Time: " + timeTaken + " ms");
+		
+		return tab;
+	}
+	
+	/**
+	 * Sort an array of numbers using Selection sort
+	 * 
+	 * @param tab Array of numbers we want to sort
+	 * @param verbose Boolean, if we want to see the tab at every iteration
+	 * @param time Boolean, if we want to know the time it took
+	 * @return Tab sorted
+	 */
+	public static int[] selectionSort(int[] tab, boolean verbose, boolean time) {
+		long startTime = System.currentTimeMillis();
+
+		while (!isSorted(tab)) {
+			for (int i = 0; i < tab.length; i++) {
+				if (verbose)
+					System.out.println(Arrays.toString(tab));
+				int min = tab[i];
+				int minIndex = i;
+				for (int j = i + 1; j < tab.length; j++) {
+					if (tab[j] < min) {
+						min = tab[j];
+						minIndex = j;
+					}
+				}
+				if (minIndex != i) {
+					tab[minIndex] = tab[i];
+					tab[i] = min;
 				}
 			}
 		}
